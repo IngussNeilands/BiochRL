0a99a6e7a0023b802daa212e36155afd7462756d
tankorsmash <tankorsmash@gmail.com>
2014-06-24 23:46:04 -0400

    added some nonsense lines

--- FirstCPPApplication/civilian.cpp
+++ FirstCPPApplication/civilian.cpp
@@ -15,6 +15,18 @@ Civilian::Civilian()
     this->chat_lines->push_back("You know what, I don't care for you at all.");
     this->chat_lines->push_back("If you had any sense at all, you'd be happy to let me touch you.");
     this->chat_lines->push_back("I'm pretty sure I'm trapped in here.");
+    this->chat_lines->push_back("When will the forest speak?"); //S
+    this->chat_lines->push_back("Listen to the man, he must have a plan."); //S
+    this->chat_lines->push_back("Show me and make me a smile I can wear."); //S
+    this->chat_lines->push_back("Brotherhood is nothing without your brothers."); //WCAR
+    this->chat_lines->push_back("Let my words be your words, let my thoughts be your thoughts."); //ABR
+    this->chat_lines->push_back("Run in circles to show your affection to the Myld God");
+    this->chat_lines->push_back("Take care not to say too much, lest you anger the Dragon.");
+    this->chat_lines->push_back("If you are me and I am you, who is he?");
+    this->chat_lines->push_back("Could there be anything more to life after he returns?");
+    this->chat_lines->push_back("Pray to the God you think is above, the one who will save you and show you his love. But now you are mine, dismembered and rotten, dug in your grave"); //RF, sorta
+    this->chat_lines->push_back("Older things are always better."); //RF, sorta
+    this->chat_lines->push_back("Consume not the orb, respect not your responsibilities; trust in your instincts and the candies.");
 }
 
 Civilian::~Civilian()
