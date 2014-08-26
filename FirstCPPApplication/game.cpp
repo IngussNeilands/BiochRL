25a6a6abd68cf18aabb3addf51bcb3862e2886df
tankorsmash <tankorsmash@gmail.com>
2014-08-25 22:05:30 -0400

    fix respawning having the same maps

--- FirstCPPApplication/game.cpp
+++ FirstCPPApplication/game.cpp
@@ -1291,6 +1291,7 @@ void Game::specify_player()
 void Game::start_game()
 {
     std::cout << "starting world gen" << std::endl;
+    Game::atlas->clear();
     Map* new_map = Game::build_world(1);
     // Map* new_map = Game::build_town();
     std::cout << "ending world gen" << std::endl;
